7583c426d0167d086a669dd93b920392d9b292fe
mijia <1137551712@qq.com>
2022-10-21 09:18:53 +0800

    10/21/22

---
+++ vue_test/src/App.vue
@@ -0,0 +1,41 @@
+<<template>
+    <div>
+        <School></School>
+        <Studnet></Studnet>
+    </div>
+</template>
+
+<script>
+
+    //引入组件
+    import School from './components/School.vue'
+    import Student from './components/Student'
+
+
+export default {
+    name: 'App',
+    template:`App`,
+    componets:{
+        School,
+        Student,
+    },
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>学生名字：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name:"School",
+        data(){
+            return {
+                name:'张三',
+                age:'18'
+            }
+        },
+    }    
+</script>
+
