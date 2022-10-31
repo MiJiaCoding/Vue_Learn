ceaf8f1166285f5032d3bd2e3cc9fdaad0da6ce0
mijia <1137551712@qq.com>
2022-10-31 21:19:08 +0800

    10/31/22

---
+++ search/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <Search/>
+    <List/>
+  </div>
+</template>
+
+<script>
+// import './assets/css/bootstrap.css'
+import Search from './components/Search.vue'
+import List from './components/List.vue'
+export default {
+  name: 'App',
+  components: {
+   Search,
+   List
+  }
+}
+</script>
+
+
